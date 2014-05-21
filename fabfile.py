397de3f9f227c85701c030aeeb4a9206e2aa89eb
m_usachev <m_usachev@wargaming.net>
2014-05-21 14:12:33 +0000

    WGNPF-3842: Fixed fabric. Added environment settings.
    git-svn-id: https://svn.wargaming.net/svn/WOT/web/wgnps/teamcity-monitor@424344 93af18ca-8a01-524a-b514-bc6650421195

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+from contextlib import nested
 
 from fabric.api import cd, run, settings, local, put
-from fabric.context_managers import hide
+from fabric.context_managers import prefix, hide
 
 __all__ = (
     'deploy',
@@ -17,9 +18,9 @@ VENV_DIR = os.path.join(SOURCES_DIR, 'venv')
 def deploy(port=8000):
     stop()
     purge_sources()
+    upload_sources()
     setup_virtual_environment()
     install_python_packages()
-    upload_sources()
     start(port)
 
 
@@ -28,7 +29,7 @@ def start(port):
 
     python_path = os.path.join(VENV_DIR, 'bin', 'python')
 
-    with cd(SOURCES_DIR):
+    with nested(cd(SOURCES_DIR), prefix('. settings.env')):
         run('dtach -n `mktemp -u /tmp/teamcity-monitor.XXX` %s server.py --port=%s' % \
             (python_path, port))
 
@@ -36,9 +37,8 @@ def start(port):
 def stop():
     print 'Stop teamcity-monitor...'
 
-    with settings(warn_only=True):
-        with hide('everything'):
-            run("kill -9 `ps -ef | grep 'server.py' | grep -v grep | awk '{print $2}'`")
+    with nested(settings(warn_only=True), hide('everything')):
+        run("kill -9 `ps -ef | grep 'server.py' | grep -v grep | awk '{print $2}'`")
 
 
 def purge_sources():
@@ -53,31 +53,27 @@ def upload_sources():
 
     tarball = 'sources.tar'
 
-    with hide('commands'):
-        with settings(warn_only=True):
-            local('rm -rf %s' % tarball)
-            local('tar --exclude="*.pyc" -cf %s .' % tarball)
+    with nested(hide('commands'), settings(warn_only=True)):
+        local('rm -rf %s' % tarball)
+        local('tar --exclude="*.pyc" -cf %s .' % tarball)
 
-            put(tarball, SOURCES_DIR)
+        run('mkdir -p %s' % SOURCES_DIR)
+        put(tarball, SOURCES_DIR)
 
-            with cd(SOURCES_DIR):
-                with settings(warn_only=True):
-                    run('tar -xf %s' % tarball)
+        with cd(SOURCES_DIR):
+            with settings(warn_only=True):
+                run('tar -xf %s' % tarball)
 
-            run('rm %s' % os.path.join(SOURCES_DIR, tarball))
+        run('rm %s' % os.path.join(SOURCES_DIR, tarball))
 
-        local('rm -rf ./%s' % tarball)
+    local('rm -rf ./%s' % tarball)
 
 
 def setup_virtual_environment():
     print 'Setup virtual environment...'
 
-    with hide('commands'):
-        run('mkdir -p %s' % SOURCES_DIR)
-        run('mkdir -p %s' % VENV_DIR)
-
-        with cd(SOURCES_DIR):
-            run('virtualenv --no-site-packages --python=python2.6 venv')
+    with nested(hide('commands'), cd(SOURCES_DIR)):
+        run('virtualenv --no-site-packages --python=python2.6 venv')
 
 
 def install_python_packages():
