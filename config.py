22a67887bb4a39e2a616104cf84689fefe23e1bd
m_usachev <m_usachev@wargaming.net>
2014-05-22 07:22:59 +0000

    WGNPF-3842: Updated WGNPS config.
    git-svn-id: https://svn.wargaming.net/svn/WOT/web/wgnps/teamcity-monitor@424531 93af18ca-8a01-524a-b514-bc6650421195

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 BUILDS_LAYOUT = [
-    [{'id': 'bt362'}, {'id': 'bt360'}, {'id': 'bt363'}, {'id': 'Wgnps_UnittestTrunkPsui'}],
+    [{'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'}],
+    [{'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}],
+    [{'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}],
+    [{'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}],
     [{'id': 'Wgnps_UnittestPsfc'},],
-    [{'id': 'Wgnps_UnittestStablePsa'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_UnittestStablePsui'}],
-    [{'id': 'bt409'}],
 ]
