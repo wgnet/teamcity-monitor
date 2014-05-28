8ff875c1de6b12b1013242a41e422f09ce2f9d83
m_usachev <m_usachev@wargaming.net>
2014-05-28 13:14:32 +0000

    Added PSA and PSO sandbox builds.
    git-svn-id: https://svn.wargaming.net/svn/WOT/web/wgnps/teamcity-monitor@427204 93af18ca-8a01-524a-b514-bc6650421195

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BUILDS_LAYOUT = [
     [{'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
-    [{'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}],
-    [{'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}],
+    [{'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}, {'id': 'StagingBuilds_Psa_PsaSnd'}],
+    [{'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}, {'id': 'StagingBuilds_Pso_PsoSnd'}],
     [{'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}],
     [{'id': 'Wgnps_UnittestPsfc'},],
 ]
