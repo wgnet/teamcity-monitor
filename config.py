744bd32f58b353b4d4175546cddec41801c9df89
m_usachev <m_usachev@wargaming.net>
2014-05-28 08:38:05 +0000

    Added sandbox to monitoring.
    git-svn-id: https://svn.wargaming.net/svn/WOT/web/wgnps/teamcity-monitor@426950 93af18ca-8a01-524a-b514-bc6650421195

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BUILDS_LAYOUT = [
-    [{'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'}],
+    [{'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
     [{'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}],
     [{'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}],
     [{'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}],
