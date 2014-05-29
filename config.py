23e34a12d271c9439250f5a3943b7d6293655c7c
m_usachev <m_usachev@wargaming.net>
2014-05-29 09:38:14 +0000

    Added PEP8 builds.
    git-svn-id: https://svn.wargaming.net/svn/WOT/web/wgnps/teamcity-monitor@427508 93af18ca-8a01-524a-b514-bc6650421195

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BUILDS_LAYOUT = [
-    [{'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
-    [{'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}, {'id': 'StagingBuilds_Psa_PsaSnd'}],
-    [{'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}, {'id': 'StagingBuilds_Pso_PsoSnd'}],
-    [{'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}],
-    [{'id': 'Wgnps_UnittestPsfc'},],
+    [{'id': 'Wgnps_Pep8psa'}, {'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'}],
+    [{'id': 'Wgnps_Pep8pso'}, {'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}],
+    [{'id': 'Wgnps_Pep8pss'}, {'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}],
+    [{'id': 'Wgnps_Pep8psui'}, {'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
+    [{'id': 'Wgnps_Pep8psfc'}, {'id': 'Wgnps_UnittestPsfc'}, {'id': 'StagingBuilds_Psa_PsaSnd'}, {'id': 'StagingBuilds_Pso_PsoSnd'}],
 ]
