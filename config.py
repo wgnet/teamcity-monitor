bc62d7ab2074ba5160ffec04d99103c46631e59b
m_usachev <m_usachev@wargaming.net>
2014-06-16 11:58:09 +0000

    Updated WGNPS config.
    git-svn-id: https://svn.wargaming.net/svn/WOT/web/wgnps/teamcity-monitor@433895 93af18ca-8a01-524a-b514-bc6650421195

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 BUILDS_LAYOUT = [
-    [{'id': 'Wgnps_Pep8psa'}, {'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'}],
-    [{'id': 'Wgnps_Pep8pso'}, {'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}],
-    [{'id': 'Wgnps_Pep8pss'}, {'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}],
-    [{'id': 'Wgnps_Pep8psui'}, {'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
-    [{'id': 'Wgnps_Pep8psfc'}, {'id': 'Wgnps_UnittestPsfc'}, {'id': 'StagingBuilds_Psa_PsaSnd'}, {'id': 'StagingBuilds_Pso_PsoSnd'}],
+    [{'id': 'Wgnps_Pep8psa'}, {'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt409'},  {'id': 'StagingBuilds_Psa_PsaSnd'}],
+    [{'id': 'Wgnps_Pep8pso'}, {'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'Wgnps_DeployStableStage11012824'}, {'id': 'StagingBuilds_Pso_PsoSnd'}],
+    [{'id': 'Wgnps_Pep8pss'}, {'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'Wgnps_DeployTrunkStage10128218'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
+    [{'id': 'Wgnps_Pep8psui'}, {'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}, {'id': 'StagingBuilds_Psui_PsuiSnd'}],
+    [{'id': 'Wgnps_Pep8psfc'}, {'id': 'Wgnps_UnittestPsfc'}],
 ]
 
 COVERAGE_BUILDS = ['bt362', 'bt360', 'bt363', 'Wgnps_UnittestTrunkPsui',
