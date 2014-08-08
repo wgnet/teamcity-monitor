9c2cd211af0dd613e2d6b0a99ad98dade7dcb6dd
Max Usachev <maxusachev@gmail.com>
2014-08-08 12:55:12 +0300

    Updated WGNPS config.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 BUILDS_LAYOUT = [
-    [{'id': 'Wgnps_Pep8psa'}, {'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt519'}, {'id': 'StagingBuilds_PsaWgs1'}, {'id': 'StagingBuilds_Psa_PsaSnd'}],
-    [{'id': 'Wgnps_Pep8pso'}, {'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'bt520'}, {'id': 'StagingBuilds_PsoWgs1'}, {'id': 'StagingBuilds_Pso_PsoSnd'}],
-    [{'id': 'Wgnps_Pep8pss'}, {'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'bt521'}, {'id': 'StagingBuilds_PssWgs1'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
-    [{'id': 'Wgnps_Pep8psui'}, {'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}, {'id': 'bt522'}, {'id': 'StagingBuilds_PsuiWgs1'}, {'id': 'StagingBuilds_Psui_PsuiSnd'}],
-    [{'id': 'Wgnps_Pep8psfc'}, {'id': 'Wgnps_UnittestPsfc'}],
+    [{'id': 'bt362'}, {'id': 'Wgnps_UnittestStablePsa'}, {'id': 'bt519'}, {'id': 'StagingBuilds_PsaWgs1'}, {'id': 'StagingBuilds_Psa_PsaSnd'}],
+    [{'id': 'bt360'}, {'id': 'Wgnps_UnittestStablePso'}, {'id': 'bt520'}, {'id': 'StagingBuilds_PsoWgs1'}, {'id': 'StagingBuilds_Pso_PsoSnd'}],
+    [{'id': 'bt363'}, {'id': 'Wgnps_UnittestStablePss'}, {'id': 'bt521'}, {'id': 'StagingBuilds_PssWgs1'}, {'id': 'StagingBuilds_Pss_PssxSnd'}],
+    [{'id': 'Wgnps_UnittestTrunkPsui'}, {'id': 'Wgnps_UnittestStablePsui'}, {'id': 'bt522'}, {'id': 'StagingBuilds_PsuiWgs1'}, {'id': 'StagingBuilds_Psui_PsuiSnd'}],
+    [{'id': ''}, {'id': 'Wgnps_UnittestPsfc'}],
     [{'id': ''}],
     [{'id': 'bt409'}, {'id': 'Wgnps_AtDevelop'}]
 ]
